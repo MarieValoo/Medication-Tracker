658ccceba6d52f7c117ab49974b4f19ac0cdbd75
NatalieM1102 <njmason511@gmail.com>
2025-05-11 13:39:48 -0400

    Update health_journal.py

--- health_journal.py
+++ health_journal.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #can ya'll see this
 
 #yes 
@@ -92,37 +91,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-#can ya'll see this
-
-#yes 
-
-import datetime
-
-class SendReminders:
-    def __init__(self, user):
-        self.user = user
-        self.schedule = self.generate_schedule()
-## Function that generate schedules for multiple different medications or just one large schedule
-## function with an If statement that checks if reminder should be sent
-
-
-
-
-def send(self, reminder):
-    user = reminder['user']
-    med = reminder['medication']
-    print(f"Hi {user}, time to take {med}.")
-## Maybe make it more specific like their dose?
-=======
-#Health Journal Class
-#Marie Valouiski
-#INST326 Final Project
-
-class healthJournal:
-    """take in medication name, dosage, frequency, and amount of days to create full journal of 
-    what it would look like if the user took all the medication according to the prescription directions
-    Attributes:
-        self
-        Output of Registers Medication"""
->>>>>>> Stashed changes
